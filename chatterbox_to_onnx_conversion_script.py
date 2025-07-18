f447d2a83da5389916a7c0bdb1adaf8d8d6001d8
Vladislav <vladislav.bronzov@gmail.com>
2025-07-18 20:32:46 +0200

    combine speech encoder and embedder, small fixes for flow

--- chatterbox_to_onnx_conversion_script.py
+++ chatterbox_to_onnx_conversion_script.py
@@ -1,4 +1,4 @@
-# !pip install --upgrade onnx==1.17.0 onnxruntime==1.20.1 onnxslim==0.1.48 transformers==4.48.3 chatterbox-tts==0.1.2
+# # !pip install --upgrade onnx==1.17.0 onnxruntime==1.20.1 onnxslim==0.1.48 transformers==4.48.3 chatterbox-tts==0.1.2
 
 import torch
 from torch import nn
@@ -12,21 +12,21 @@ SOS = SPEECH_VOCAB_SIZE
 EOS = SPEECH_VOCAB_SIZE + 1
 
 
-def create_dummy_inputs(hp, batch_size=1, text_len=40):
+def create_dummy_inputs(hp, batch_size=2, text_len=40):
     speaker_emb = torch.randn(batch_size, hp.speaker_embed_size)
     cond_prompt_speech_tokens = torch.randint(
         0, hp.speech_tokens_dict_size, (batch_size, hp.speech_cond_prompt_len)
     )
     emotion_adv = torch.tensor([0.5] * batch_size).unsqueeze(1)
-    text_tokens = torch.randint(
-        0, hp.text_tokens_dict_size, (2, text_len)
-    ) #original code uses torch.atleast_2d(text_tokens)
+    text_tokens_ids = torch.randint(
+        0, hp.text_tokens_dict_size, (batch_size, text_len)
+    )
     sot = hp.start_text_token
     eot = hp.stop_text_token
-    text_tokens = F.pad(text_tokens, (1, 0), value=sot)
-    text_tokens = F.pad(text_tokens, (0, 1), value=eot)
-    speech_tokens = hp.start_speech_token * torch.ones_like(text_tokens[:, :1])
-    return speaker_emb, cond_prompt_speech_tokens, emotion_adv, text_tokens, speech_tokens
+    text_tokens_ids = F.pad(text_tokens_ids, (1, 0), value=sot)
+    text_tokens_ids = F.pad(text_tokens_ids, (0, 1), value=eot)
+    speech_input_ids = hp.start_speech_token * torch.ones_like(text_tokens_ids[:, :1])
+    return speaker_emb, cond_prompt_speech_tokens, emotion_adv, text_tokens_ids, speech_input_ids
 
 model = ChatterboxTTS.from_pretrained(device="cpu")
 config = model.t3.hp
@@ -34,8 +34,8 @@ config = model.t3.hp
 output_dir = "converted"
 os.makedirs(output_dir, exist_ok=True)
 
-# 1. Export embedder
-class Embedder(nn.Module):
+# 1. Export speech encoder
+class SpeechEncoder(nn.Module):
     def __init__(self):
         super().__init__()
         self.cond_enc = model.t3.cond_enc
@@ -44,13 +44,14 @@ class Embedder(nn.Module):
         self.speech_pos_emb = model.t3.speech_pos_emb
         self.text_pos_emb = model.t3.text_pos_emb
         self.hp = model.t3.hp
+        self.register_buffer("bos_token", torch.tensor([[self.hp.start_speech_token]], dtype=torch.long))
 
     def forward(self, 
                 speaker_emb, 
                 cond_prompt_speech_tokens, 
                 emotion_adv,
-                text_tokens,
-                speech_tokens):
+                text_tokens_ids,
+                speech_input_ids):
         t3_cond = T3Cond(
             speaker_emb=speaker_emb,
             clap_emb=None,
@@ -58,125 +59,74 @@ class Embedder(nn.Module):
             cond_prompt_speech_emb=None,
             emotion_adv=emotion_adv
             )
-        
+        text_tokens_ids = torch.atleast_2d(text_tokens_ids)
         t3_cond.cond_prompt_speech_emb = self.speech_emb(t3_cond.cond_prompt_speech_tokens) + \
             self.speech_pos_emb(t3_cond.cond_prompt_speech_tokens)
         cond_emb =  self.cond_enc(t3_cond)
-        text_emb = self.text_emb(text_tokens)
-        speech_emb = self.speech_emb(speech_tokens)
+        text_emb = self.text_emb(text_tokens_ids)
+        speech_emb = self.speech_emb(speech_input_ids)
         if self.hp.input_pos_emb == "learned":
-            text_emb = text_emb + self.text_pos_emb(text_tokens)
-            speech_emb = speech_emb + self.speech_pos_emb(speech_tokens)
+            text_emb = text_emb + self.text_pos_emb(text_tokens_ids)
+            speech_emb = speech_emb + self.speech_pos_emb(speech_input_ids)
         len_cond = cond_emb.size(1)
 
         if cond_emb.size(0) != text_emb.size(0):
              cond_emb = cond_emb.expand(text_emb.size(0), -1, -1)
 
         # concat
-        embeds = torch.stack([
+        inputs_embeds = torch.stack([
             torch.cat((ce, te, se))
             for ce, te, se in zip(cond_emb, text_emb, speech_emb)
         ])  # (B, length, dim)
-        return embeds, len_cond
-
-speaker_emb, cond_prompt_speech_tokens, emotion_adv, text_tokens, speech_tokens = create_dummy_inputs(config)
-embedder_model = Embedder()
-torch.onnx.export(
-    embedder_model,
-    (speaker_emb, cond_prompt_speech_tokens, emotion_adv, text_tokens, speech_tokens),
-    f"{output_dir}/t3_embedder.onnx",
-    export_params=True,
-    opset_version=14,
-    do_constant_folding=True,
-    input_names=["speaker_emb", "cond_prompt_speech_tokens", "emotion_adv", "text_tokens", "speech_tokens"],
-    output_names=["embeds", "len_cond"],
-    dynamic_axes={
-        "speaker_emb": {0: "batch_size"},
-        "cond_prompt_speech_tokens": {0: "batch_size", 1: "seq_len_cond"},
-        "emotion_adv": {0: "batch_size"},
-        "text_tokens": {0: "batch_size", 1: "seq_len_text"},
-        "speech_tokens": {0: "batch_size", 1: "seq_len_speech"},
-        "embeds": {0: "batch_size", 1: "sequence_length"},
-        "len_cond": {0: "batch_size"}
-    },
-)
-print(f"✅ Embedder ONNX export is completed. Model saved as 't3_embedder.onnx'")
-
-t3_cond = T3Cond(
-    speaker_emb=speaker_emb,
-    clap_emb=None,
-    cond_prompt_speech_tokens=cond_prompt_speech_tokens,
-    cond_prompt_speech_emb=None,
-    emotion_adv=emotion_adv
-)
-
-# 2. Export speech encoder
-class SpeechEncoder(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.speech_emb = model.t3.speech_emb
-        self.speech_pos_emb = model.t3.speech_pos_emb.emb
-    
-    def forward(self, bos_token, embeds, idx=0):
-        bos_embed = self.speech_emb(bos_token)
+        bos_embed = self.speech_emb(self.bos_token)
+        idx = torch.tensor(0)
         idx = torch.atleast_2d(idx)
         idx =  self.speech_pos_emb(idx)
         bos_embed = bos_embed + idx
         bos_embed = torch.cat([bos_embed, bos_embed])
-        inputs_embeds = torch.cat([embeds, bos_embed], dim=1)
-        return inputs_embeds  # (B, T, dim)
-bos_token = torch.tensor([[config.start_speech_token]], dtype=torch.long)
-embeds, _ = model.t3.prepare_input_embeds(
-            t3_cond=t3_cond,
-            text_tokens=text_tokens,
-            speech_tokens=speech_tokens,
-            cfg_weight=0.5,
-        )
-idx = torch.tensor(0)
-speech_encoder = SpeechEncoder()
+        inputs_embeds = torch.cat([inputs_embeds, bos_embed], dim=1)
+        return inputs_embeds, len_cond
+
+speaker_emb, cond_prompt_speech_tokens, emotion_adv, text_tokens_ids, speech_input_ids = create_dummy_inputs(config)
+embedder_model = SpeechEncoder()
 torch.onnx.export(
-    speech_encoder,
-    (bos_token, embeds, idx),
-    f"{output_dir}/t3_speech_encoder.onnx",
+    embedder_model,
+    (speaker_emb, cond_prompt_speech_tokens, emotion_adv, text_tokens_ids, speech_input_ids),
+    f"{output_dir}/speech_encoder.onnx",
     export_params=True,
     opset_version=14,
     do_constant_folding=True,
-    input_names=["bos_token", "embeds", "idx"],
-    output_names=["inputs_embeds"],
+    input_names=["speaker_emb", "cond_prompt_speech_tokens", "emotion_adv", "text_tokens_ids", "speech_input_ids"],
+    output_names=["inputs_embeds", "len_cond"],
     dynamic_axes={
-        "bos_embed": {0: "batch_size", 1: "sequence_length"},
-        "embeds": {0: "batch_size", 1: "sequence_length"},
+        "speaker_emb": {0: "batch_size"},
+        "cond_prompt_speech_tokens": {0: "batch_size", 1: "seq_len_cond"},
+        "emotion_adv": {0: "batch_size"},
+        "text_tokens_ids": {0: "batch_size", 1: "seq_len_text"},
+        "speech_input_ids": {0: "batch_size", 1: "seq_len_speech"},
+        "inputs_embeds": {0: "batch_size", 1: "sequence_length"}
     },
 )
-print(f"✅ Encoder ONNX export is completed. Model saved as 't3_speech_encoder.onnx'")
+print(f"✅ Speech Encoder ONNX export is completed. Model saved as 'speech_encoder.onnx'")
 
-# 3. Export LLM Backbone (Llama3 from repo vladislavbro/llama_backbone_0.5) using https://github.com/microsoft/onnxruntime-genai/blob/main/src/python/py/models/builder.py
+# 2. Export LLM Backbone (Llama3 from repo vladislavbro/llama_backbone_0.5) using https://github.com/microsoft/onnxruntime-genai/blob/main/src/python/py/models/builder.py
+# Before export, we replace embed_tokens and lm_head of the LLM backbone model
+# because it does not specifically use lllm tokens
 
-# 4. Export Speech head
-speach_head = model.t3.speech_head
-hidden_states = torch.randn(1, text_tokens.shape[1], config.n_channels)
-torch.onnx.export(
-    speach_head,
-    (hidden_states),
-    f"{output_dir}/t3_speech_head.onnx",
-    export_params=True,
-    opset_version=14,
-    do_constant_folding=True,
-    input_names=["hidden_states"],
-    output_names=["logits"],
-    dynamic_axes={
-        "hidden_states": {0: "batch_size", 1: "sequence_length"},
-    },
-)
-print(f"✅ Speech head ONNX export is completed. Model saved as 't3_speech_head.onnx'")
+# model.t3.tfmr.embed_tokens.weight = torch.nn.Parameter(model.t3.speech_head.weight.contiguous())
+# model.t3.cfg.vocab_size = model.t3.speech_head.out_features
+# model_to_exp = LlamaForCausalLM(model.t3.cfg)
+# model_to_exp.lm_head = model.t3.speech_head
+# model_to_exp.model = model.t3.tfmr
 
-head_out = speach_head(hidden_states)
+hidden_states = torch.randn(1, text_tokens_ids.shape[1], config.n_channels)
+head_out = model.t3.speech_head(hidden_states)
 
 speech_tokens = torch.randint(
-        0, config.text_tokens_dict_size, (1, head_out.shape[1])
+        0, config.text_tokens_dict_size, (head_out.shape[1],)
     )
 
-# 5. Export Flow inference wrapper
+# 3. Export Flow inference wrapper
 class FlowInferenceWrapper(nn.Module):
     def __init__(self):
         super().__init__()
@@ -275,6 +225,7 @@ torch.onnx.export(
     input_names=["speech_tokens"],
     output_names=["mel_len1", "mu", "mask", "spks", "cond"],
     dynamic_axes={
+        "speech_tokens": {0: "sequence_len"},
         "mu": {0: "batch_size", 2: "output_time"},
         "mask": {0: "batch_size", 2: "output_time"},
         "spks": {0: "batch_size"},
@@ -283,7 +234,7 @@ torch.onnx.export(
 )
 print(f"✅ Flow inference ONNX export is completed. Model saved as 'flow_inference.onnx'")
 
-# 6. Export Conditional Decoder
+# 4. Export Conditional Decoder
 class ConditionalDecoder(nn.Module):
     def __init__(self):
         super().__init__()
@@ -355,7 +306,7 @@ torch.onnx.export(
 )
 print(f"✅ Conditional decoder ONNX export is completed. Model saved as 'conditional_decoder.onnx'")
 
-# 7. Post-processing
+# 5. Post-processing
 for f in os.listdir(output_dir):
     p = os.path.join(output_dir, f)
     onnxslim.slim(p, p)
